abb2f25d3407ffa752b6e35c5dfd52dcefff37f8
cmurch98 <cmurchie48@gmail.com>
2020-11-13 13:00:07 +1100

    Fixed the sed substitution string

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -2,12 +2,12 @@
 echo -e "Welcome to setup_bashrc.sh!\nThis script will add some pre-defined aliases and modify some default .bashrc parameters." | fold -s
 
 echo "Inserting aliases..."
-sed -ri "/#alias l='ls -CF'/a alias fu='sudo apt -qq update && sudo apt -qq upgrade -y'" .bashrc
+sed -ri "/#alias l='ls -CF'/a alias fu='sudo apt -qq update && sudo apt -qq upgrade -y'" /home/pi/.bashrc
 echo "Alias 'fu' (Full Upgrade) Complete"
 echo "Done!"
 
 echo "Updating bash history duplicates..."
-sed -i "s/HISTCONTROL=ignoreboth/HISTCONTROL=erasedups" .bashrc
+sed -i "s/HISTCONTROL=ignoreboth/HISTCONTROL=erasedups/" /home/pi/.bashrc
 echo "Done!"
 
 echo -e "----------------------------------------\nsetup_bashrc.sh complete!\n"
